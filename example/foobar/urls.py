db4330df43210c7551458be8a6fd6b8899aed984
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-01-05 10:33:11 +0100

    more docs on xslt

--- example/foobar/urls.py
+++ example/foobar/urls.py
@@ -3,5 +3,6 @@ import foobar.views
 
 urlpatterns = patterns('',
     (r'^$', foobar.views.index),
+    (r'^chain$', foobar.views.chain),
     (r'^xslt$', foobar.views.xslt),
 )
--- example/foobar/views.py
+++ example/foobar/views.py
@@ -8,11 +8,18 @@ from easymode.xslt.response import render_to_response
 from foobar import models as foobar_models
 
 def index(request):
-    """index view function"""
+    """shows untransformed hierarchical xml output"""
     foos = foobar_models.Foo.objects.all()
     return HttpResponse(tree.xml(foos), mimetype='text/xml')
+
+def chain(request):
+    """shows how the XmlQuerySetChain can be used instead of @toxml decorator"""
+    bars = foobar_models.Bar.objects.all()
+    qsc = XmlQuerySetChain(bars)
+    return HttpResponse(tree.xml(qsc))
     
 def xslt(request):
-    """test xml output view function"""
+    """Shows xml output transformed with standard xslt"""
     foos = foobar_models.Foo.objects.all()
     return render_to_response('xslt/model-to-xml.xsl', foos)
+
