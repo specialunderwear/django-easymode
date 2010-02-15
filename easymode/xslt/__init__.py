15d78d7fc447439ee5834e94fdb1a59b7e619bda
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-02-15 14:30:34 +0100

    Code that determines which xslt engine to use is only executed once.

--- easymode/xslt/__init__.py
+++ easymode/xslt/__init__.py
@@ -2,6 +2,7 @@
 Contains xslt transformation functionality to be used with django models
 """
 import codecs
+from StringIO import StringIO
 from django.utils.encoding import smart_unicode
 
 __all__ = ('XsltError', 'transform', 'prepare_string_param')
@@ -19,9 +20,11 @@ class MessageHandler:
         return self.content
 
 def _transform_libxslt(xml, xslt, params=None):
-    import libxslt
-    import libxml2
+    """
+    Do xml transformation using libxml2-python and libxslt-python.
     
+    http://xmlsoft.org/python.html
+    """
     try:
         xslt_doc = libxml2.parseDoc(xslt)
         xslt_proc = libxslt.parseStylesheetDoc(xslt_doc)
@@ -39,9 +42,12 @@ def _transform_libxslt(xml, xslt, params=None):
     except RuntimeError as e:
         raise XsltError(str(e))
 
-def _transform_libxsltmod(xml, xslt, params):
-    import libxsltmod
+def _transform_libxsltmod(xml, xslt, params=None):
+    """
+    Do xslt transformation using libxsltmod.
     
+    http://www.rexx.com/~dkuhlman/libxsltmod.html
+    """
     handler = MessageHandler()
     try:
         result = libxsltmod.translate_to_string(
@@ -53,39 +59,43 @@ def _transform_libxsltmod(xml, xslt, params):
         messages = handler.getContent()
         raise XsltError(messages)
 
-def _transform_lxml(xml, xslt, params):
+def _transform_lxml(xml, xslt, params=None):
+    """
+    Do xslt transformation using lxml.
+    
+    http://codespeak.net/lxml/
+    """
+    # assuming params where created with prepare_string_param,
+    # they are encoded as unicode, which lxml does not like
+    for (key, value) in params.iteritems():
+        params[key] = value.decode('utf-8')
+
+    xml_doc = etree.fromstring(xml)
+    xslt_doc = etree.fromstring(xslt)
+    xslt_proc = etree.XSLT(xslt_doc)
+    
+    result = xslt_proc(xml_doc, **params)
+    return unicode(result)
+
+# determine which xslt engine to use,
+# the default is libxsltmod.
+transform = _transform_libxsltmod
+
+try:
+    import libxslt
+    import libxml2
+
+    transform = _transform_libxslt
+except:
+    pass
+
+try:
     from lxml import etree
-    from StringIO import StringIO
-    
-    try:
-        # assuming params where created with prepare_string_param,
-        # they are encode as unicode, which lxml does not like
-        for (key, value) in params.iteritems():
-            params[key] = value.decode('utf-8')
 
-        xml_doc = etree.fromstring(xml)
-        xslt_doc = etree.fromstring(xslt)
-        xslt_proc = etree.XSLT(xslt_doc)
-        
-        result = xslt_proc(xml_doc, **params)
-        return unicode(result)
-    except RuntimeError as e:
-        raise XsltError(str(e))
-    
-def transform(xml, xslt, params=None):
-    """transform the xml using the xslt"""
-    try:
-        return _transform_libxslt(xml, xslt, params)
-    except ImportError:
-        pass
-    
-    try:
-        return _transform_lxml(xml, xslt, params)
-    except ImportError:
-        pass
-        
-    return _transform_libxsltmod(xml, xslt, params)
-
+    transform = _transform_lxml
+except:
+    pass
+   
 def prepare_string_param(string):    
     result = u"'%s'" % (string.replace("'","&apos;") or '')
     return result.encode('utf-8')
