d062db00eb9fd2076ae873e66ffeb83e8a58d520
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-02-26 00:27:47 +0100

    Added manifest and setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+packages = []
+data_files = []
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+for dirpath, dirnames, filenames in os.walk('easymode'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+setup(name='django-easymode',
+      version='0.2.1',
+      description='A toolkit for making xml based flash websites with django',
+      author='L. van de Kerkhof',
+      author_email='lars@permanentmarkers.nl',
+      url='http://github.com/LUKKIEN/django-easymode/',
+      packages=packages,
+      data_files=data_files,
+     )
