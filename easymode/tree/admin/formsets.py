0150e2e12863a097b1197a1655006a3ccd0651d3
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-02-28 19:12:09 +0100

    fixed some bugs pylint found

--- easymode/tree/admin/formsets.py
+++ easymode/tree/admin/formsets.py
@@ -7,7 +7,7 @@ from django.forms import IntegerField
 from django.forms.formsets import BaseFormSet
 from django.utils.translation import ugettext_lazy as _
 
-from widgets.foreignkey import RenderLink
+from easymode.tree.admin.widgets.foreignkey import RenderLink
 
 class VisiblePrimaryKeyFormset(BaseModelFormSet):
     """
@@ -26,6 +26,7 @@ class VisiblePrimaryKeyFormset(BaseModelFormSet):
                 pk_value = None
 
         attrs = dict(app_label=self.model._meta.app_label, modelname=self.model.__name__.lower(), label=unicode(form.instance))
-        form.fields[self._pk_field.name] = IntegerField( initial=pk_value, required=False, widget=RenderLink(attrs=attrs), label=_("Navigate to:"))
+        form.fields[self._pk_field.name] = IntegerField( initial=pk_value, required=False, 
+                                                         widget=RenderLink(attrs=attrs), label=_("Navigate to:"))
 
         BaseFormSet.add_fields(self, form, index)
