5828f12ce30edb7b4cc04e9ce6b2eaf96636b686
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-05-30 19:16:43 +0200

    Improved drag and drop admin model base.

--- easymode/admin/abstract.py
+++ easymode/admin/abstract.py
@@ -4,13 +4,8 @@ class AbstractOrderedModel(models.Model):
     """
     This model includes functionality for ordering.
     """
-    order = models.PositiveIntegerField()
-    
-    def save(self, force_insert=False, force_update=False):
-        # this is the last model in the list except if there are 10000000 models.
-        order = 99999999
-        super(AbstractOrderedModel, self).save(False, False)
-        
+    order = models.AutoField()
+            
     class Meta:
         abstract = True
         ordering = ('order',)
