1fdb21cd48d5177d63463cc137c5efcf033e0ea3
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-07-20 13:43:58 +0200

    AutoField unfortunately can not be used for non foreign key fields, so AbstractOrderedModel will be as it was.

--- easymode/admin/abstract.py
+++ easymode/admin/abstract.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-class AbstractOrderedModel(models.Model):    
+class AbstractOrderedModel(models.Model):
     """
     This model includes functionality for ordering.
     """
-    order = models.AutoField()
-            
+    
+    order = models.PositiveIntegerField()
+    
+    def save(self, force_insert=False, force_update=False):
+        # this is the last model in the list except if there are 10000000 models.
+        order = 99999999
+        super(AbstractOrderedModel, self).save(False, False)
+        
     class Meta:
         abstract = True
         ordering = ('order',)
-    
-
-
-    
-
-        
