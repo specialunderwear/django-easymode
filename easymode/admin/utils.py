82ba49c709696fb971b1baf108ccc8a1e4367408
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-07-22 16:40:23 +0200

    Added option to exclude models from register_all

--- easymode/admin/utils.py
+++ easymode/admin/utils.py
@@ -6,17 +6,18 @@ from easymode.i18n.admin.decorators import L10n
 
 __all__ = ('register_all',)
 
-def register_all(module):
+def register_all(module, exclude=[]):
     """
     Register all models in _module_ with the admin
     
     :param module: The module that contains the models
+    :param exclude: A list of models classes that should not be registered
     """
     member_names = dir(module)
 
     for member_name in member_names:
         obj = getattr(module, member_name, None)
-        if isinstance(obj, ModelBase):
+        if isinstance(obj, ModelBase) and obj not in exclude:
             try:
                 if hasattr(obj, 'localized_fields'):
                     admin.site.register(obj, L10n(obj, admin.ModelAdmin))
