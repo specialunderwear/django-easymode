56acc14c5cf77aba250565c80518dfbd26e15722
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-07-27 14:50:20 +0200

    some renaming

--- easymode/i18n/meta.py
+++ easymode/i18n/meta.py
@@ -1,6 +1,7 @@
 """
 Derived from django-transmeta.
-Contains mach to make models internationalized.
+Contains machinery to change a type after construction,
+by using a class decorator.
 
 This in contrast with django-transmeta which requires
 overriding the __metaclass__.
@@ -215,27 +216,27 @@ def localize_fields(cls, localized_fields):
     for field in localized_fields:
         original_attr = cls._meta.get_field_by_name(field)[0]
         
-        for cnt, lang_code in enumerate(get_all_language_codes()):
-            lang_attr = copy.copy(original_attr)
+        for cnt, language_code in enumerate(get_all_language_codes()):
+            i18n_attr = copy.copy(original_attr)
             # add support for south introspection.
-            lang_attr._south_introspects = True
-            lang_attr.original_fieldname = field
-            lang_attr.include_in_xml = False
-            lang_attr_name = get_real_fieldname(field, lang_code)
-            lang_attr.creation_counter = lang_attr.creation_counter + .01 * cnt
+            i18n_attr._south_introspects = True
+            i18n_attr.original_fieldname = field
+            i18n_attr.include_in_xml = False
+            lang_attr_name = get_real_fieldname(field, language_code)
+            i18n_attr.creation_counter = i18n_attr.creation_counter + .01 * cnt
             # null must be allowed for the message id language because this language
             # might not be available at all in the backend
-            if not lang_attr.null and lang_attr.default is NOT_PROVIDED:
-                lang_attr.null = True
+            if not i18n_attr.null and i18n_attr.default is NOT_PROVIDED:
+                i18n_attr.null = True
             
-            if lang_code != msgid_language:
+            if language_code != msgid_language:
                 # no validation for the fields that are language specific
-                if not lang_attr.blank:
-                    lang_attr.blank = True
+                if not i18n_attr.blank:
+                    i18n_attr.blank = True
                     
-            if lang_attr.verbose_name:
-                lang_attr.verbose_name = translation.string_concat(lang_attr.verbose_name, u' (%s)' % lang_code)
-            cls.add_to_class(lang_attr_name, lang_attr)
+            if i18n_attr.verbose_name:
+                i18n_attr.verbose_name = translation.string_concat(i18n_attr.verbose_name, u' (%s)' % language_code)
+            cls.add_to_class(lang_attr_name, i18n_attr)
             
         # delete original field
         del cls._meta.local_fields[cls._meta.local_fields.index(original_attr)]
