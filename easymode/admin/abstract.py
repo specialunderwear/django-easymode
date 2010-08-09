126fd9f70fc5f0d5d3ced14b9b832b30663036a4
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-08-09 20:33:28 +0200

    AbstractOrderedModel needs tests.

--- easymode/admin/abstract.py
+++ easymode/admin/abstract.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 class AbstractOrderedModel(models.Model):
@@ -5,11 +6,11 @@ class AbstractOrderedModel(models.Model):
     This model includes functionality for ordering.
     """
     
-    order = models.PositiveIntegerField()
+    order = models.PositiveIntegerField(_('Order'), default=99999999)
     
     def save(self, force_insert=False, force_update=False):
         # this is the last model in the list except if there are 10000000 models.
-        order = 99999999
+        self.order = 99999999
         super(AbstractOrderedModel, self).save(False, False)
         
     class Meta:
