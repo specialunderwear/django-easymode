61c272b79f7c89a5c287a3603aab30087facf61b
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-08-26 13:29:56 +0200

    insert_draft now turns a revision into a tree, like RecursiveXmlSerializer and inserts that entire tree with all related items into the xml, instead of only the top level node.

--- easymode/easypublisher/utils.py
+++ easymode/easypublisher/utils.py
@@ -9,6 +9,86 @@ from easymode.tree.serializers import RecursiveXmlSerializer
 
 __all__ = ('filter_unpublished', 'insert_draft')
 
+class RevisionTree(object):
+    """
+    This class builds an xml tree from a revision, just
+    like :class:`~easymode.tree.serializers.RecursiveXmlSerializer` does
+    from a regular model.
+    
+    :attr topnode: The main node in the revision, with all child nodes nested inside.
+    :attr nodelist: All nodes in the revision in a flat list.
+    :meth xpath_for_node: Return an xpath statement which can be used to look
+        in another xml document for the same node.
+    """
+    
+    def __init__(self, revision_id):
+        nodelist = []
+
+        revision = Revision.objects.get(pk=revision_id)
+        serializer = RecursiveXmlSerializer()
+        
+        # turn all version objects into lxml elements
+        for ver in revision.version_set.all():
+            model_instance = ver.get_object_version().object
+            draft_xml = serializer.serialize([model_instance])
+            draft_doc = etree.fromstring(draft_xml)
+            draft_nodes = draft_doc.xpath(r'/django-objects/object')
+            nodelist += draft_nodes
+        
+        topnode = self._find_topnode(nodelist)
+        nodelist.remove(topnode)
+
+        self.nodelist = nodelist
+        # create xml topnode with nested items
+        self._append_children(topnode, nodelist)
+        self.topnode = topnode
+
+    def xpath_for_node(self, node):
+        return r"//object[@pk='%(pk)s' and @model='%(model)s']" \
+            % {
+            'pk': node.get('pk'),
+            'model': node.get('model'),
+        }
+        
+    def _get_items_by_model_and_pk(self,nodelist, model, pk):
+        object_filter = lambda x: x.get('model') == model and x.get('pk') == pk
+        return filter(object_filter, nodelist)
+
+    def _get_relations(self, node):
+        return map(lambda x: (x.get('to'), x.text), 
+            node.xpath(r"field[@to and text() != 'None']"))
+
+    def _get_parents(self, node, nodelist):
+        result = []
+        for (model, pk) in self._get_relations(node):
+            result += self._get_items_by_model_and_pk(nodelist, model, pk)
+        
+        return result
+
+    def _get_children(self, node, nodelist):
+        children = []
+        pk = node.get('pk')
+        model = node.get('model')
+        
+        return [childnode for childnode in nodelist if \
+            (model, pk) in self._get_relations(childnode)]
+
+    def _find_topnode(self, nodelist):
+        for node in nodelist:
+            parents = self._get_parents(node, nodelist)
+            if len(parents) == 0:
+                return node
+            else:
+                return self._find_topnode(parents)
+                
+        return None
+
+    def _append_children(self, node, nodelist):
+        for child in self._get_children(node, nodelist):
+            self._append_children(child, nodelist)
+            node.append(child)
+
+
 def insert_draft(revision_id, xml):
     """
     Insert the data belonging to revision revision_id into the xml
@@ -17,29 +97,26 @@ def insert_draft(revision_id, xml):
     :param xml: A string that can be parsed as valid xml.
     :result: An xml string with the revision data included.
     """
-    rev = Revision.objects.get(pk=revision_id)
-    xml_doc = etree.fromstring(xml)    
-    serializer = RecursiveXmlSerializer()    
 
-    for ver in rev.version_set.all():
-        model_instance = ver.get_object_version().object
-        
-        xpath_params = {
-            'app_label' : ver.content_type.app_label,
-            'model' : ver.content_type.model,
-            'pk': model_instance.pk,
-        }
-        
-        xpath = r"//object[@pk='%(pk)s' and @model='%(app_label)s.%(model)s']" \
-            % xpath_params
+    revision_tree = RevisionTree(revision_id)
+    xml_doc = etree.fromstring(xml)
 
-        draft_xml = serializer.serialize([model_instance])
-        draft_doc = etree.fromstring(draft_xml)
-        
-        draft_node = draft_doc.xpath(r'/django-objects/object')[0]
-        for node in xml_doc.xpath(xpath):
-            node.getparent().replace(node, draft_node)
-        
+    # each separated versioned object should be replaced separately, because
+    # for example list_editable view changes have many unrelated objects in 
+    # a single revision
+    for draft_node in revision_tree.nodelist:
+        for matching_node in xml_doc.xpath(revision_tree.xpath_for_node(draft_node)):
+            matching_node.getparent().replace(matching_node, draft_node)
+
+    # when the revision is normal and not from a list_editable view, there is one
+    # top node and the rest are related items. Create a tree structure with all
+    # the related items as a tree, like easymode's recursive xml serializer does
+    # as well.
+    top_node = revision_tree.topnode
+    
+    for matching_node in xml_doc.xpath(revision_tree.xpath_for_node(top_node)):
+        matching_node.getparent().replace(matching_node, top_node)
+    
     return etree.tostring(xml_doc)
 
 
