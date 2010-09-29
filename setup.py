c539254bc6f6f72265995699839d8f73ee5120ec
Lars van de Kerkhof <lars@permanentmarkers.nl>
2010-09-29 16:01:21 +0200

    release 0.13.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ documentation at http://packages.python.org/django-easymode/
 
 release notes at http://packages.python.org/django-easymode/changes.html"""
 
-version = '0.13.3'
+version = '0.13.4'
 packages = []
 data_files = []
 
